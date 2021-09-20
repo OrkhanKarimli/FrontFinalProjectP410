4be863fd8f436158e2e7aad4833b54a80bc245ef
OrkhanKarimli <orkhansk@code.edu.az>
2021-09-20 23:16:51 +0400

    Set loading page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+    $("header").addClass("container,headmenu")
   $(".nice-select").niceSelect();
+  $(".loader1").load("_loading.html");
   $("header").load("_header.html");
+  
+  setTimeout(function(){ 
+     
+    $("body").removeClass("overflow-y");
+    $(".loader1").addClass("none");
+  }, 2000);
 });
