4d4c508ee525a47ccbdf0bec177ae6b59af41c5e
OrkhanKarimli <orkhansk@code.edu.az>
2021-09-21 05:54:20 +0400

    download_our_app

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,36 @@ $(document).ready(function () {
     $("body").removeClass("overflow-y");
     $(".loader1").addClass("none");
   }, 2000);
-  
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true,
+            loop:true,
+            autoplay:true,
+            autoplayTimeout:2000
+        },
+        600:{
+            items:1,
+            nav:false,
+            loop:true,
+            autoplay:true,
+            autoplayTimeout:2000
+        },
+        1000:{
+            items:1,
+            nav:false,
+            loop:true,
+            dots:true,
+            autoplay:true,
+            autoplayTimeout:2000
+        }
+    }
+})
 });
 
 
