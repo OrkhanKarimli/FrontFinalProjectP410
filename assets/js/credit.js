1da303dac964f68809f0218a1f99ac38f6d699b9
OrkhanKarimli <orkhansk@code.edu.az>
2021-09-26 15:28:41 +0400

    calculator fixed

--- assets/js/credit.js
+++ assets/js/credit.js
@@ -70,7 +70,7 @@ $(document).ready(function () {
       let downresult = price_value * 0.3;
       let price_final_value=price_value-down_value;
       console.log(price_final_value);
-      let total =  price_final_value + ( price_final_value * rate_value * month_value) / 100;
+      let total =  price_final_value + ( price_final_value * rate_value) /month_value ;
       let monthly_payment = total / month_value;
       if (down_value < downresult) {
         alert("Ilkin ödəniş manlın dəyərinin 30%-dən çoxunu təşkil etməlidir");
